02aa794b8e98ed350d582188edacf9cc9d4b97b9
Tiana Hayden <tiana.hayden@me.com>
2018-10-12 14:57:31 -0700

    Initial commit. Server file and views folder setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080')
+});
